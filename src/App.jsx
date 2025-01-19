b278b3dab6df75bd7f8d9ce6bdf7ed31f23290af
Taposh paul <taposhp05@gmail.com>
2025-01-20 02:11:10 +0600

    create navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Header from './components/header/Header'
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<h1 className='text-2xl font-sans font-bold text-center text-orange-700'>Home</h1 >} />
@@ -14,7 +14,7 @@ function App() {
         <Route path="/about" element={<h1 className='text-2xl font-sans font-bold text-center text-orange-700'>About</h1 >} />
         <Route path="/contact" element={<h1 className='text-2xl font-sans font-bold text-center text-orange-700'>Contact</h1 >} />
       </Routes>
-    </div>
+    </>
   )
 }
 
--- src/components/header/Navber.jsx
+++ src/components/header/Navber.jsx
@@ -1,17 +1,64 @@
-import React from 'react'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 function Navber() {
+    const [isOpen, setIsOpen] = useState(false)
+
+
+    const menuLinks = [
+        {
+            name: 'Home',
+            link: '/'
+        },
+        {
+            name: 'Contact',
+            link: '/contact'
+        },
+        {
+            name: 'About',
+            link: '/about'
+        },
+        {
+            name: 'Sign Up',
+            link: '/sign Up'
+        }
+
+
+    ]
+
     return (
-        <div className='md:flex hidden justify-end gap-12 text-base font-poppins font-normal leading-6 text-center text-text2'>
-            <NavLink to='/' >
-                Home
-                {/* <hr className='hover:w-full bg-text1 h-[2px] border-none rounded-full focus:outline-none'></hr> */}
-            </NavLink>
-            <NavLink to='/contact' >Contact</NavLink>
-            <NavLink to='/about' >About</NavLink>
-            <NavLink to='/sign Up' >Sign Up</NavLink>
-        </div>
+        <>
+            {/* big screen menu  */}
+            <div className='md:flex hidden justify-end lg:gap-12 gap-5 text-base font-poppins font-normal leading-6 text-center text-text2'>
+
+                {
+                    menuLinks.map((item, index) => <NavLink key={index} to={item.link} >{item.name} </NavLink>)
+                }
+
+            </div>
+
+            {/* small screen menu toggle bars icon*/}
+
+            <div onClick={() => setIsOpen(!isOpen)} className='md:hidden block text-[22px] text-text2'>
+                <FontAwesomeIcon icon={faBars} />
+            </div>
+
+            {/* small screen menu  */}
+            <div className={`md:hidden flex flex-col justify-start bg-primary fixed w-full h-screen top-0 left-0 right-0 z-10 pt-12 gap-6 text-base font-poppins font-normal leading-6 text-center text-text2 
+                 duration-300 ease-in-out -translate-x-full ${isOpen && "translate-x-0"}`}>
+                {
+                    menuLinks.map((item, index) => <NavLink onClick={() => setIsOpen(!isOpen)} key={index} to={item.link} >{item.name} </NavLink>)
+                }
+
+                {/* mobile menu toggle bars icon*/}
+
+                <div onClick={() => setIsOpen(!isOpen)} className=' fixed top-5 right-5 block text-[22px] text-button2'>
+                    <FontAwesomeIcon icon={faXmark} />
+                </div>
+            </div>
+        </>
     )
 }
 
