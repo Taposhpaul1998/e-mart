33e1a00fa3745767486fb3108493fb6fee42c3f2
Taposh paul <taposhp05@gmail.com>
2025-02-16 00:26:25 +0600

    upgrede tailwind css

--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SearchBar() {
     return (
         <div className='flex justify-start items-center lg:gap-[34px] gap-5 bg-secondary md:pl-5 pl-3 py-[7px] pr-3'>
-            <input type="text" placeholder='What are you looking for?' className='md:w-[153px] w-32 bg-secondary focus:outline-none md:text-xs text-[10px] leading-[18px]' />
+            <input type="text" placeholder='What are you looking for?' className='md:w-[153px] w-32 bg-secondary focus:outline-hidden md:text-xs text-[10px] leading-[18px]' />
             <svg className='cursor-pointer' width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M17 17L13.2223 13.2156M15.3158 8.15789C15.3158 10.0563 14.5617 11.8769 13.2193 13.2193C11.8769 14.5617 10.0563 15.3158 8.15789 15.3158C6.2595 15.3158 4.43886 14.5617 3.0965 13.2193C1.75413 11.8769 1 10.0563 1 8.15789C1 6.2595 1.75413 4.43886 3.0965 3.0965C4.43886 1.75413 6.2595 1 8.15789 1C10.0563 1 11.8769 1.75413 13.2193 3.0965C14.5617 4.43886 15.3158 6.2595 15.3158 8.15789V8.15789Z" stroke="black" stroke-width="1.5" stroke-linecap="round" />
             </svg>
--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -7,40 +7,34 @@ import 'swiper/css';
 
 function Hero() {
 
-
     return (
-        < Swiper
-            autoplay={{ delay: 5000 }}
-            loop={true}
-        >
-            <SwiperSlide>
-                <div className='mt-10 ml-[45px] bg-black flex justify-between w-[892px]'>
-                    <div className='mt-[58px] ml-16 mb-[47px]'>
-                        <div className='text-text text-base font-normal font-poppins flex justify-start items-center gap-6 mb-5'>
-                            <img src={apolelogo} alt="" />
-                            <span>iPhone 14 Series</span>
-                        </div>
-                        <div className='text-text text-5xl leading-[60px] font-poppins font-semibold mb-[22px] xl:w-[290px] w-60'>
-                            <h1 className='leading-[60px]'>
-                                Up to 10% off Voucher
-                            </h1>
-                        </div>
-                        <Link to='/shop' className=' flex justify-start items-center gap-[11px]'>
-                            <span className='text-text text-base font-medium font-poppins relative border-b border-text'>Shop Now</span>
-                            <svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1.5 8H18M18 8L11 1M18 8L11 15" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                        </Link>
+        <div>
+            <div className='mt-10 ml-[45px] bg-black flex justify-between w-[892px]'>
+                <div className='mt-[58px] ml-16 mb-[47px]'>
+                    <div className='text-text text-base font-normal font-poppins flex justify-start items-center gap-6 mb-5'>
+                        <img src={apolelogo} alt="" />
+                        <span>iPhone 14 Series</span>
                     </div>
-                    <div className='relative h-full w-full overflow-hidden z-10'>
-                        <div className='absolute right-0 top-4 z-50'>
-                            <img src={heroImg} alt="hero image" />
-                        </div>
-
+                    <div className='text-text text-5xl leading-[60px] font-poppins font-semibold mb-[22px] xl:w-[290px] w-60'>
+                        <h1 className='leading-[60px]'>
+                            Up to 10% off Voucher
+                        </h1>
                     </div>
+                    <Link to='/shop' className=' flex justify-start items-center gap-[11px]'>
+                        <span className='text-text text-base font-medium font-poppins relative border-b border-text'>Shop Now</span>
+                        <svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.5 8H18M18 8L11 1M18 8L11 15" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </Link>
                 </div>
-            </SwiperSlide>
-        </Swiper>
+                <div className='relative h-full w-full overflow-hidden z-10'>
+                    <div className='absolute right-0 top-4 z-50'>
+                        <img src={heroImg} alt="hero image" />
+                    </div>
+
+                </div>
+            </div>
+        </div>
 
     )
 }
